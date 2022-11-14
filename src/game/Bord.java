41e6381d9f56461cfe6e3e1be3c3b7e66661446b
alan-h-utbm <84342588+alan-h-utbm@users.noreply.github.com>
2022-11-14 09:48:01 +0100

    Replaced Position array with Position Object

--- src/game/Bord.java
+++ src/game/Bord.java
@@ -6,24 +6,24 @@ import menu.WindowProperties;
 
 public class Bord implements WindowProperties{
 	
-	private int[] position;
+	private Position position_m;
 	private Score score;
 	private Grid grid;
 	private PlayGrid playGrid;
 	
 	public Bord(int line, int column) {
-		position = new int[2];
-		position[0] = line;
-		position[1] = column;
-		score = new Score(position);
-		grid = new Grid(position);
-		playGrid = new PlayGrid(position);
+		
+		position_m = new Position(column, line);
+		
+		score = new Score(position_m);
+		grid = new Grid(position_m);
+		playGrid = new PlayGrid(position_m);
 	}
 	
 	public void draw(Graphics g) {
 		Color bord = new Color(193,189,180);
 		g.setColor(bord);
-		g.fillRect(position[1] * BORD_SIZE * 2, position[0] * BORD_SIZE * 2, BORD_SIZE, BORD_SIZE);
+		g.fillRect(position_m.getX() * BORD_SIZE * 2, position_m.getY() * BORD_SIZE * 2, BORD_SIZE, BORD_SIZE);
 		
 		score.draw(g);
 		grid.draw(g);
