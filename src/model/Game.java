298427ad16532888bb9dbdb881513792ca508224
gilles <gilles.maurer@utbm.fr>
2022-12-28 10:31:36 +0100

    add comments 2

--- src/model/Game.java
+++ src/model/Game.java
@@ -4,52 +4,73 @@ import java.util.LinkedList;
 
 import controller.Controller;
 
+// ATTENTION : dans game on crée toujours 4 bord est-ce normal ?
+// Toujours une fonction test
+
+// déclaration de la classe Game
 public class Game {
 	
+	// Centre de la table 
 	private Pot pot;
+	
+	// Plateau de jeu pour chaque joueur 
 	private Bord[] players;
+	
+	// Le contrôleur qui gère le déroulement du jeu
 	private Controller controller;
 	
+	// L'indice du joueur actuellement actif
 	private int current_player;
 	
+	
+	// Constructeur qui prend en paramètre un contrôleur et le nombre de joueurs
 	public Game(Controller ref, int nb_player) {
 		
 		current_player = 0;
-		
 		controller = ref;
 		
-		
+		// initialise les Bord avec le nombre de joueurs
 		players = new Bord[4];
 		for(int i = 0; i<4; i++) players[i] = new Bord(i, this);
 		
+		// initialise le Pot en fonction du nombre de joueurs 
 		pot = new Pot(4, this);
-		
 		
 	}
 	
+	
+	// Envoie une liste de tuiles à la main du joueur actuel
 	public void sendSelectiontoBord(LinkedList<Tile> tiles) {
 		players[current_player].setHand(tiles);
 	}
 	
+	
+	// Envoie une tuile à la main du joueur actuel
 	public void sendSelectiontoBord(Tile tile) {
 		players[current_player].setHand(tile);
 	}
 	
+	
+	// Envoie une liste de tuiles au Bag
 	public void sendToBag(LinkedList<Tile> tiles) {
 		pot.sendToBag(tiles);
 	}
 	
+	
+	// Indique la fin d'un tour de jeu et passe au joueur suivant
 	public void endOfSet() {
 		
-		
+		// Appelle la méthode endOfSet() de chaque joueur
 		for(Bord p: players) {
 			p.endOfSet();
 		}
 
+		// passe au joueur suivant 
 		current_player++;
 	
-	
 	}
+	
+	
 	
 	public void test() {
 		pot.test();
@@ -59,10 +80,14 @@ public class Game {
 		
 	}
 
+	
+	
 	public void sendSelectiontoBordTest(LinkedList<Tile> tiles_bord) {
 		players[current_player].test(tiles_bord);
 		
 	}
+	
+	
 	
 	public LinkedList<Tile> getTilesToView(int index){
 		System.out.print("game - ");
--- src/model/Line.java
+++ src/model/Line.java
@@ -3,21 +3,35 @@ package model;
 import java.util.LinkedList;
 
 
-
+// Classe représentant une "Line"
 public class Line {
 
+    // Tableau de "Tile" représentant la "Line"
 	private Tile[] linearray;
+	
+    // Première case encore libre de la "Line"
 	private int current_index;
+	
+    // Taille de la "Line"
 	private int size;
+
+    // Tableau de booléens indiquant si chaque couleur est présente ou non dans la ligne
 	private boolean[] color_presence;
 
-	
+    // Couleur courante de la ligne	
 	private ColorEnum current_color;
+	
+    // Instance de la classe Malus
 	private Malus malus_m;
+	
+    // Instance de la classe Pattern
 	private Pattern pattern_m;
 		
+	// 
 	private LinkedList<Tile> to_send;
 	
+	
+	// Constructeur de la classe Line
 	public Line(Malus malus_p, Pattern pattern_ref ,int size) {
 		
 		malus_m = malus_p;
@@ -36,76 +50,13 @@ public class Line {
 			color_presence[i]=false;
 		}
 		
+		// initialise la "Line"
 		initArray(size);
-		
-		
-		
-	}
-	
-	public void addChoice(LinkedList<Tile> tiles) {
-		
-		int i = current_index;
-		for(Tile p: tiles) {
-			if(i<size) {
-					//addColor(p);
-				setTileIndex(i, p);
-				i++;
-			}else {
-				malus_m.addTile(p);
-			}
-			current_index = i;
-				
-		}
-		
-		
-	}
-	
-	public boolean checkFull() {
-		return current_index == size;
-	}
-	
-	public void endOfSet() {
-		if (checkFull()) 
-			pattern_m.determineSendingPlace(size, linearray[0]);
-	
-	}
-	
-	public void addChoice(Tile tile) {
-		
-			setTileIndex(current_index, tile);
-			
-			current_index++;
-		
-	}
-	
-	public void addColor(Tile tile_p) {
-		
-		color_presence[tile_p.getColorEnum().ordinal()] = true;
-	}
-	
-	public boolean isPossible(Tile tile) {
-		return !checkColor(tile) &&(tile.getColorEnum()==current_color || current_color == null);
-	}
-	
-	public boolean checkColor(Tile tile_p) {
-		return color_presence[tile_p.getColorEnum().ordinal()];
-	}
-	
-	public Tile getTileIndex(int index) {
-		return linearray[index];
-	}
-	
-	public void setTileIndex(int index, Tile tile_p){
-		if(index<size) {
-			current_color = tile_p.getColorEnum();
-			linearray[index] = tile_p;
-			//System.out.print("added ");
-		}else {
-			malus_m.addTile(tile_p);
-		}
 		
 	}
 
+	
+	// initialise la "Line"
 	private void initArray(int index) {
 		linearray = new Tile[index];
 		/*for(int i =0; i<index; i++) {
@@ -114,10 +65,88 @@ public class Line {
 		size = index;
 	}
 	
+	
+	// permet d'ajouter une liste de "Tile" à la "Line"
+	public void addChoice(LinkedList<Tile> tiles) {
+		
+		int i = current_index;
+		for(Tile p: tiles) {
+			if(i<size) {
+					//addColor(p);
+				setTileIndex(i, p);
+				i++;
+			}else { // les "Tile" en trop sont mise dans le malus 
+				malus_m.addTile(p);
+			}
+			current_index = i;
+				
+		}
+	}
+	
+	
+	// permet d'ajouter une "Tile" à la "Line"
+	public void addChoice(Tile tile) {
+		
+			setTileIndex(current_index, tile);
+			current_index++;
+	}
+	
+	
+	// check si la ligne est pleine
+	public boolean checkFull() {
+		return current_index == size;
+	}
+	
+	
+	// ajoute la couleur dans les couleurs qui ont déjà été présente
+	public void addColor(Tile tile_p) {
+		
+		color_presence[tile_p.getColorEnum().ordinal()] = true;
+	}
+	
+	// check s'il est possible de placer un ou plusieurs "Tile" de la couleur de celle en parametre dans le "Line"
+	public boolean isPossible(Tile tile) {
+		return !checkColor(tile) && (tile.getColorEnum()==current_color || current_color == null);
+	}
+	
+	
+	// check si la couleur a déjà été mise dans cette "Line"
+	public boolean checkColor(Tile tile_p) {
+		return color_presence[tile_p.getColorEnum().ordinal()];
+	}
+	
+	
+	// renvoie la "Tile" présente à un index donné
+	public Tile getTileIndex(int index) {
+		return linearray[index];
+	}
+	
+	
+	// permet d'ajouter une "Tile" à l'index donné
+	public void setTileIndex(int index, Tile tile_p){
+		
+		if(index<size) {
+			current_color = tile_p.getColorEnum();
+			linearray[index] = tile_p;
+			//System.out.print("added ");
+	
+		} else {
+			malus_m.addTile(tile_p);
+		}
+	}
+
+	
+	// renvoie le tableau de "Tile" de la "Line"
 	public Tile[] getTiles() {
 		return this.linearray;
 	}
 	
+	public Tile[] getLine() {
+		return this.linearray;
+	}
+	
+	
+	// affiche la "Line"
 	public void display() {
 		int i =0;
 		while(i<this.current_index) {
@@ -135,6 +164,8 @@ public class Line {
 		
 	}
 	
+	
+	// permet de clear la "Line" pour être prêt à acceuillir d'autres "Tile"
 	public LinkedList<Tile> clear() {
 		to_send.clear();
 		pattern_m.determineSendingPlace(size - 1, linearray[0]);
@@ -151,8 +182,11 @@ public class Line {
 		return to_send;
 	}
 	
-	public Tile[] getLine() {
-		return this.linearray;
+	
+	// en fin de manche, si la ligne est pleine on place une "Tile" correspondante dans "Pattern"
+	public void endOfSet() {
+		if (checkFull()) 
+			pattern_m.determineSendingPlace(size, linearray[0]);
 	}
 	
 	
